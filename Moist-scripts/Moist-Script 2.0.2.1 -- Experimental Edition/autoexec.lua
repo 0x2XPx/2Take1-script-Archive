b32ac62a4ee7fcde78ca6de601dfc45f93af681e
0x2XPx <79924985+0x2XPx@users.noreply.github.com>
2022-06-25 02:03:20 +0200

    Delete Moist-scripts directory

--- Moist-scripts/Moist-Script 2.0.2.1 -- Experimental Edition/autoexec.lua
+++
@@ -1,9 +0,0 @@
-function MoistLoad()
-local rootPath = utils.get_appdata_path("PopstarDevs", "2Take1Menu")
-	local file = rootPath .. "\\scripts\\MoistScript.lua"
-
-	local f = assert(loadfile(file)) return f()
-
-end
-
-MoistLoad()
